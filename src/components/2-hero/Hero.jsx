b113aea4a1202fa2717ee4d00feb5bf321684817
ziadmohamed <ziad@gmail.com>
2025-03-09 00:04:21 +0200

    React_Portofolio

---
+++ src/components/2-hero/Hero.jsx
@@ -0,0 +1,70 @@
+import Lottie from "lottie-react";
+import "./hero.css";
+import devAnimation from "../../animation/dev.json";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+  const lottieRef = useRef();
+
+  return (
+    <section className="hero flex" id="about">
+      <div className="left-section  ">
+        <div className="parent-avatar flex">
+          <motion.img
+            initial={{ transform: "scale(0)" }}
+            animate={{ transform: "scale(1.1)" }}
+            transition={{ damping: 6, type: "spring", stiffness: 100 }}
+            src="./me-modified.png"
+            className="avatar"
+            alt=""
+          />
+          <div className="icon-verified"></div>
+        </div>
+
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className="title"
+        >
+          Hi, I'm Ziad Mohamed Web Developer.
+        </motion.h1>
+
+        <p className="sub-title">
+          I'm a skilled web developer specializing in building modern web
+          applications with React and Node.js. With a strong focus on
+          performance, and user experience, I create full-stack solutions that
+          are both functional and visually engaging.
+        </p>
+
+        <div className="all-icons flex">
+          <a href="">
+            <div className="icon icon-instagram"></div>
+          </a>
+          <a href="https://github.com/pigpis-Curwud-0wikty?tab=repositories">
+            <div className="icon icon-github"></div>
+          </a>
+          <a href="www.linkedin.com/in/ziad-mohamed-b01482345">
+            <div className="icon icon-linkedin"></div>
+          </a>
+        </div>
+      </div>
+
+      <div className="right-section animation ">
+        <Lottie
+          lottieRef={lottieRef}
+          className=""
+          onLoadedImages={() => {
+            // @ts-ignore
+            // https://lottiereact.com/
+            lottieRef.current.setSpeed(0.5);
+          }}
+          animationData={devAnimation}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
